c40689014c15da4126cdb7911bfdb5482fccbdd2
Shalini Murallee <shalinimurallee@gmail.com>
2024-05-15 18:43:50 +1000

    plotted beds needed til Dec

--- COVID19 wk11.R
+++ COVID19 wk11.R
@@ -1,1 +1,11 @@
-setwd()
+setwd('C:/Users/shali/OneDrive/Documents/gitt/tmp_git')
+covid = read.csv("epiSEIHCRD_combAge.csv")
+covid$t=as.Date(covid$t,origin = '2020-07-01')
+covid$t
+
+covid_sub=subset(covid,subset = (t>='2020-07-01' & t<='2020-12-31'))
+covid_sub
+
+plot(covid_sub$H)
+
+
